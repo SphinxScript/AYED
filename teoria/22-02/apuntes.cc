40f73543a0853a483fa28e7fe3846729e11ed9d9
alu0101643137 <alu0101643137>
2024-02-22 12:33:05 +0000

    last

--- teoria/22-02/apuntes.cc
+++ teoria/22-02/apuntes.cc
@@ -8,3 +8,35 @@ int main() {
     res += nz_val * v.at(nz_inx);
   }
 }
+
+
+// siguiente parte:
+
+class Vector {
+ private:
+  double *v_;
+  int sz_;
+ public:
+  Vector() = default;
+  Vector(const int sz) : sz_(sz) {}
+};
+
+class Complex {
+ private:
+  double r_;
+  double i_;
+ public:
+  Complex() = default;
+  Complex(const double r, const double i) : r_(r), i_(i) {}
+};
+
+
+int main() {
+  Complex a(5, 3);
+  Complex b(2, 6);
+  Vector a(25);
+  Vector b(16);
+  for (int i = 0; i < 25; ++i) {
+    a.at(i) = 25.3 + i;
+  }
+}
