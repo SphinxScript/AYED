80c18cbb36ff1c09d3278d735802ac2f8d0c84ad
alu0101643137 <alu0101643137>
2024-03-04 11:01:24 +0000

    last

---
+++ problemas/4-3-24.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  cout << "Elementos de la submatriz triangular inferior sin diagonal: \n";
+  bool is_symmetric() {
+    bool is_sym = true;
+    int m{};
+    for (int i{0}; i < m; ++i) {
+      for (int j{0}; j < i; ++j) {
+        if(C[i][j] != C[j][i]) {
+          is_sim = false;
+        }
+      }
+    }
+    return is_sym;
+  }
+  return 0;
+}
