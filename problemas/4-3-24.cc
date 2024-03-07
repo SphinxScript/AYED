a988bdf568fb7009481b73de557113362cd2acad
alu0101643137 <alu0101643137>
2024-03-07 16:22:51 +0000

    last

--- problemas/4-3-24.cc
+++ problemas/4-3-24.cc
@@ -1,3 +1,5 @@
+/*
+
 #include <iostream>
 
 using namespace std;
@@ -18,3 +20,47 @@ int main() {
   }
   return 0;
 }
+
+
+
+
+void scalar_product() {
+  double producto = 0;
+  for (int i{0}; i < nz_; ++i) {
+    producto += val_[i] * v[inx_[i]];
+
+  }
+}
+
+void product() {
+  double product = 0;
+  for (int i{0}, j{0}; (i < sv1.nz_) && (j < sv2.nz_);) {
+    if (sv1.inx_[i] == sv1.inx[j]) {
+
+    }
+  }
+  cout << product << "\n"
+}
+
+
+
+
+
+
+
+void suma_vector() {
+  double suma = 0;
+  for (int i{0}, j{0}; (i < sv1.nz_) && (j < sv2.nz_);) {
+    if (sv1.inx_[i] == sv2.inx_[j]) {
+      suma += sv1.val_[i] + sv2.val_[j];
+    }
+    else if (sv1.inx_[i] < sv2.inx_[j]) {
+      ++i;    
+    }
+    else {
+      ++j;
+    }
+  }
+}
+
+*/
